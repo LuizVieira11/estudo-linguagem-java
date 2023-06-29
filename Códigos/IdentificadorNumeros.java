6b288005d59de5a9eb04d58a6d94f5dfe0882cb2
Luiz Gustavo Santos Vieira <luiz.vieira11@outlook.com>
2023-06-29 20:31:13 -0300

    Create IdentificadorNumeros.java

---
+++ Códigos/IdentificadorNumeros.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class IdentificadorNumeros {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        int par = 0, impar = 0, pos = 0, neg = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite 5 números inteiros");
+        for(int i = 0; i < 5; i++){
+            nums[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < 5; i++){
+            if(nums[i] % 2 == 0){
+                par++;
+            }else{
+                impar++;
+            }
+
+            if(nums[i] >= 0){
+                pos++;
+            }else{
+                neg++;
+            }
+        }
+
+        System.out.printf("%n%n");
+        System.out.println("Quantidades de números:");
+        System.out.println("Ímpares: " + impar);
+        System.out.println("Pares: " + par);
+        System.out.println("Positivos: " + pos);
+        System.out.println("Negativos: " + neg);
+
+    }
+}
