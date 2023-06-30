558228d06bdbe6077ee17351b589e6049cef7e4d
Luiz Gustavo Santos Vieira <luiz.vieira11@outlook.com>
2023-06-30 14:13:08 -0300

    Create SomaIntervalos.java

---
+++ Códigos/SomaIntervalos.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SomaIntervalos {
+    public static void main(String[] args) {
+        int i, j;
+        int sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Forneça dois valores inteiros para somar os números entre eles: ");
+        i = scanner.nextInt();
+        j = scanner.nextInt();
+
+        if(i > 0 || j > 0){
+            if(i < j){
+                for( ; i <= j; i++){
+                    if(i == j){
+                        System.out.printf(i + " ");
+                    }else {
+                        System.out.printf(i + " + ");
+                    }
+                    sum += i;
+                }
+            }else if(j < i){
+                for( ; j <= i; j++){
+                    if(i == j){
+                        System.out.printf(i + " ");
+                    }else {
+                        System.out.printf(i + " + ");
+                    }
+                    sum += j;
+
+                }
+            }
+        }
+        System.out.printf("= " + sum);
+    }
+    
+}
